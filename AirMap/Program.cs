c1ca5729ff544cd12e707deeebcd85b2fc75dbc8
Dawid <dawidczernik@interia.pl>
2025-06-06 15:04:40 +0200

    Add global exemption catcher

--- AirMap/Program.cs
+++ AirMap/Program.cs
@@ -1,39 +1,41 @@
+using System.Net.NetworkInformation;
 using AirMap.Controllers;
+using AirMap.Data;
 using AirMap.Models;
 using Microsoft.EntityFrameworkCore;
-using AirMap.Data;
-using System.Net.NetworkInformation;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-var configuration = builder.Configuration;
+    var configuration = builder.Configuration;
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.Configure<SocialMediaLinks>(builder.Configuration.GetSection("SocialMediaLinks"));
+    builder.Services.AddControllersWithViews();
+    builder.Services.Configure<SocialMediaLinks>(builder.Configuration.GetSection("SocialMediaLinks"));
 
 // Rejestracja konfiguracji jako us³ugi
-builder.Services.AddSingleton(configuration);
+    builder.Services.AddSingleton(configuration);
 
 // Rejestracja HttpClient oraz serwisów do uruchomienia w tle
-builder.Services.AddHttpClient<AirQualityHostedService>(); // Rejestracja HttpClient dla HostedService
-builder.Services.AddHostedService<AirQualityHostedService>(); // Rejestracja HostedService jako singleton
-builder.Services.AddHostedService<SensorProcessor>(); // Rejestracja innego HostedService
+    builder.Services.AddHttpClient<AirQualityHostedService>(); // Rejestracja HttpClient dla HostedService
+    builder.Services.AddHostedService<AirQualityHostedService>(); // Rejestracja HostedService jako singleton
+    builder.Services.AddHostedService<SensorProcessor>(); // Rejestracja innego HostedService
 
 
 // Konfiguracja DbContext z u¿yciem SQL Server
-builder.Services.AddDbContext<AppDbContext>(
-    options =>
+    builder.Services.AddDbContext<AppDbContext>(options =>
     {
         options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection"),
             sqlServerOptions =>
-            { // przy pierwszej próbie po³¹czenia z baz¹ danych baza time-outuje - próba naprawienia poprzez retryonfailure
+            {
+                // przy pierwszej próbie po³¹czenia z baz¹ danych baza time-outuje - próba naprawienia poprzez retryonfailure
                 sqlServerOptions.EnableRetryOnFailure(
                     maxRetryCount: 10,
                     maxRetryDelay: TimeSpan.FromSeconds(30),
                     errorNumbersToAdd: null
-                    );
+                );
             }
         );
         options.LogTo(Console.WriteLine, LogLevel.Warning); // Log warnings to the console
@@ -43,40 +45,46 @@ builder.Services.AddDbContext<AppDbContext>(
 
 
 // Test po³¹czenia z baz¹ danych
-var ConStr = builder.Configuration.GetConnectionString("DefaultConnection");
-if (string.IsNullOrEmpty(ConStr))
-{
-    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    var ConStr = builder.Configuration.GetConnectionString("DefaultConnection");
+    if (string.IsNullOrEmpty(ConStr))
+    {
+        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-}
-builder.Services.AddHealthChecks()
-    .AddDbContextCheck<AppDbContext>();
+    }
 
-var app = builder.Build();
+    builder.Services.AddHealthChecks()
+        .AddDbContextCheck<AppDbContext>();
+
+    var app = builder.Build();
 
 // Konfiguracja potoku HTTP
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // Domyœlna wartoœæ HSTS: 30 dni. Mo¿esz zmieniæ to w scenariuszach produkcyjnych
-    app.UseHsts();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // Domyœlna wartoœæ HSTS: 30 dni. Mo¿esz zmieniæ to w scenariuszach produkcyjnych
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+
+    app.MapHealthChecks("/healthz");
+
+
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-app.MapHealthChecks("/healthz");
-
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
