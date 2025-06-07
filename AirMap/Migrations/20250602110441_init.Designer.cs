0877563a7822022ff27ffa1b0055784c9fca7d9c
Dawid <dawidczernik@interia.pl>
2025-06-07 12:50:40 +0200

    Supress 'lower-cased ascii characters' warning in Migrations.

--- AirMap/Migrations/20250602110441_init.Designer.cs
+++ AirMap/Migrations/20250602110441_init.Designer.cs
@@ -13,7 +13,9 @@ namespace AirMap.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     [Migration("20250602110441_init")]
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     partial class init
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
