edbefb00ee00dbbd458acb262992aeb1071452f0
Dawid <dawidczernik@interia.pl>
2025-06-24 14:04:51 +0200

    Derefer separate models from SensorModel class

---
+++ AirMap/Models/Location.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirMap.Models
+{
+    /// <summary>
+    ///     Instance of Location table values with preferred value-types.
+    /// </summary>
+    public class Location
+    {
+        /// <summary>
+        ///     Identity number of the location for auto ID within DB.
+        /// </summary>
+        [Key]
+        public long Id { get; set; }
+
+        /// <summary>
+        ///     Identity number of the location from source API.
+        /// </summary>
+        public long? SourceApiId { get; set; }
+
+        /// <summary>
+        ///     Latitude of the sensor's location.
+        /// </summary>
+        public double? Latitude { get; set; }
+
+        /// <summary>
+        ///     Longitude of the sensor's location.
+        /// </summary>
+        public double? Longitude { get; set; }
+
+        /// <summary>
+        ///     Altitude of the sensor's location.
+        /// </summary>
+        public double? Altitude { get; set; }
+
+        /// <summary>
+        ///     Country of the sensor's location.
+        /// </summary>
+        public string? Country { get; set; }
+
+        /// <summary>
+        ///     Indicates if the sensor is installed indoors.
+        /// </summary>
+        public bool? Indoor { get; set; }
+
+        /// <summary>
+        ///     Exact_location status of the sensor's location. Indicates if the location is exact or approximate.
+        /// </summary>
+        public bool? ExactLocation { get; set; }
+    }
+}
---
+++ AirMap/Models/Sensor.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirMap.Models
+{
+    /// <summary>
+    ///     Instance of Sensor class values with preferred value-types.
+    /// </summary>
+    public class Sensor
+    {
+        /// <summary>
+        ///     Identity number of the sensor for auto ID within DB.
+        /// </summary>
+        [Key]
+        public long Id { get; set; }
+
+        /// <summary>
+        ///     Identity number of the sensor from source API.
+        /// </summary>
+        public long? SourceApiId { get; set; }
+
+        /// <summary>
+        ///     Pin used by the sensor.
+        /// </summary>
+        public int? Pin { get; set; }
+
+        /// <summary>
+        ///     SensorType table reference, used for sensor type data.
+        /// </summary>
+        public long? SensorTypeId { get; set; }
+        [ForeignKey("SensorTypeId")]
+        public SensorType? SensorType { get; set; }
+    }
+}
