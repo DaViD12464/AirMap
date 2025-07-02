1b3b186435cbef97ba9b33a997a66d71fc116ce2
Dawid <dawidczernik@interia.pl>
2025-07-02 14:31:28 +0200

    Add sensor controller & endpoint to API.

---
+++ AirMap/Controllers/SensorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AirMap.Data;
+using AirMap.Helpers;
+using AirMap.Models;
+
+namespace AirMap.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SensorController : ControllerBase
+    {
+        [HttpPost("GetIconData")]
+        public ActionResult<string> GetIconData([FromBody] SensorModel sensor)
+        {
+            var iconData = AirQualityHelper.IconHelper(sensor);
+            return Ok(iconData);
+        }
+    }
+}
