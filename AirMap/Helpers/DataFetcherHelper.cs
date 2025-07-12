e1e17f0073fedaecda599fdda1dbb58e04b9915e
Dawid <dawidczernik@interia.pl>
2025-07-12 12:04:34 +0200

    Add helper compare functions.

--- AirMap/Helpers/DataFetcherHelper.cs
+++ AirMap/Helpers/DataFetcherHelper.cs
@@ -2,6 +2,17 @@
 {
     public static class DataFetcherHelper
     {
+        public static bool AreNotEqual(float? a, float? b, float tolerance = 0.0001f)
+        {
+            return Math.Abs((a ?? 0) - (b ?? 0)) > tolerance;
+        }
+
+        public static bool AreNotEqual(double? a, double? b, double tolerance = 0.0001)
+        {
+            return Math.Abs((a ?? 0) - (b ?? 0)) > tolerance;
+        }
+
+        /*  // Redundant code as CuckooSearch is Discontinued in use..
         public static long? GetUniqueId(this long? id, string test , string test2 = "dasd", string test3 = "dasd")
         {
             var result = AlgorithmsHelper.CuckooSearch();
@@ -27,5 +38,6 @@
             var v = GetUniqueId(1, test3: "test", test2: "test", test: "23");
             return 0;
         }
+        */
     }
 }
