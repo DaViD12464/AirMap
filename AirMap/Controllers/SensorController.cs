6099a9c462bce442edb694ada041644f4e3caa9b
Dawid <dawidczernik@interia.pl>
2025-07-15 01:18:42 +0200

    Add manual data fetching endpoint; Fix naming of current endpoints.

--- AirMap/Controllers/SensorController.cs
+++ AirMap/Controllers/SensorController.cs
@@ -2,18 +2,21 @@
 using AirMap.Data;
 using AirMap.Helpers;
 using AirMap.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirMap.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class SensorController : ControllerBase
+    public class SensorController(AppDbContext dbContext) : ControllerBase
     {
+        private readonly AppDbContext _dbContext = dbContext;
+
         // redundant endpoint at this point - temporarily kept for future usage
-        #region GetIconDataEndpoint 
+        #region Redundant/Deprecated
 
 
-        [HttpPost("GetIconData")]   
+        [HttpPost("GetIconData")]
         public ActionResult<string> GetIconData([FromBody] SensorModel sensor)
         {
             var iconData = AirQualityHelper.IconHelper(sensor);
@@ -23,7 +26,7 @@ namespace AirMap.Controllers
 
 
         [HttpPost("GetIconDataBatch")]
-        public ActionResult<List<object>> GetIconDataBatchWithIds([FromBody] List<SensorModel> sensors)
+        public ActionResult<List<object>> GetIconDataBatch([FromBody] List<SensorModel> sensors)
         {
             var result = sensors.Select(sensor => new {
                 sensorId = sensor.Id,
@@ -34,7 +37,7 @@ namespace AirMap.Controllers
         }
 
         [HttpPost("GetPopUpDataBatch")]
-        public ActionResult<List<object>> GetPopUpDataBatchWithIds([FromBody] List<SensorModel> sensors)
+        public ActionResult<List<object>> GetPopUpDataBatch([FromBody] List<SensorModel> sensors)
         {
             var result = sensors.Select(sensor => new
             {
@@ -43,6 +46,54 @@ namespace AirMap.Controllers
             }).ToList<object>();
             return Ok(result);
         }
+
+        [HttpGet("GetAllSensorData")]
+        public async Task<ActionResult<List<SensorModel>>> GetAllSensorData()
+        {
+            var allSensors = await _dbContext.Set<SensorModel>()
+                .Include(s => s.Location)
+                .Include(s => s.Sensor)
+                .Include(s => s.Sensor!.SensorType)
+                .Include(s => s.SensorDataValues)
+                .ToListAsync();
+
+            return Ok(allSensors);
+        }
+
+
+        [HttpGet("GetAllSensorDataManual")]
+        public async Task<ActionResult<List<SensorModel>>> GetAllSensorDataManual()
+        {
+            var allSensors = await DatabaseHelper.GetAllAsync<SensorModel>(_dbContext);
+
+            var tasks = new[]
+            {
+                DatabaseHelper.GetMissingData<SensorModel, Location>(allSensors, _dbContext),
+                DatabaseHelper.GetMissingData<SensorModel, Sensor>(allSensors, _dbContext),
+                DatabaseHelper.GetMissingDataFromList<SensorModel, SensorDataValues>(allSensors, _dbContext)
+            };
+
+            var results = await Task.WhenAll(tasks);
+
+            var modelsWithSensor = results
+                .SelectMany(list => list)
+                .Where(m => m.Sensor != null)
+                .ToList();
+
+            var sensorsFilled = await DatabaseHelper.GetMissingData<Sensor, SensorType>(
+                modelsWithSensor.Select(m => m.Sensor!).ToList(), _dbContext);
+
+            for (int i = 0; i < modelsWithSensor.Count; i++)
+            {
+                if (sensorsFilled[i] != null)
+                {
+                    modelsWithSensor[i].Sensor = sensorsFilled[i];
+                }
+            }
+
+            return Ok(results[2]);
+        }
+
 
     }
 }
