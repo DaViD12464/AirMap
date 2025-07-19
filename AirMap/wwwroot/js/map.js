e4353de332c4cb31760433b0278db9925a517497
Dawid <dawidczernik@interia.pl>
2025-07-19 21:44:31 +0200

    Remove razor data fetching; Add data fetching via JS.

--- AirMap/wwwroot/js/map.js
+++ AirMap/wwwroot/js/map.js
@@ -1,108 +1,113 @@
-﻿
-/*< !--Map Initialization Script-- >*/
-document.addEventListener("DOMContentLoaded", async function () {
-    var map = L.map('map').setView([52.1143385, 19.4236714], 7);
+﻿/*< !--Map Initialization Script-- >*/
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
+        document.addEventListener("DOMContentLoaded", async function () {
+        const sensorData = await fetch("/api/sensor/GetAllSensorData")
+        .then(res => res.json())
+        .catch(err => {
+            console.error("Error loading sensor data:", err);
+        return [];
+        });
+
+        //console.log(sensorData);
+
+        var map = L.map('map').setView([52.1143385, 19.4236714], 7);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
         attribution: '© OpenStreetMap'
-    }).addTo(map);
+        }).addTo(map);
 
-    const sensorData = JSON.parse(document.getElementById('sensorData').textContent);
-
-    const icons = {
-        veryGoodAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/VeryGoodAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        goodAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/GoodAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        moderateAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/ModerateAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        sufficientAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/SufficientAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        badAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/BadAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        veryBadAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/VeryBadAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        unknownAirQualityIcon: L.icon({ iconUrl: '/AirQualityMarkers/UnknownAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [6, -48] }),
-        defaultGreen: L.icon({ iconUrl: 'https://maps.gstatic.com/mapfiles/ms2/micons/green-dot.png', iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [6, -32] }),
-        defaultBlue: L.icon({ iconUrl: 'https://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png', iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [6, -32] }),
-    };
-    //Icon Data fetching & PopUp Data fetching
-    const [iconNames, textValues] = await Promise.all([
+        const icons = {
+            veryGoodAirQualityIcon: L.icon({iconUrl: '/AirQualityMarkers/VeryGoodAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        goodAirQualityIcon:      L.icon({iconUrl: '/AirQualityMarkers/GoodAQ.png',     iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        moderateAirQualityIcon:  L.icon({iconUrl: '/AirQualityMarkers/ModerateAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        sufficientAirQualityIcon:L.icon({iconUrl: '/AirQualityMarkers/SufficientAQ.png', iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        badAirQualityIcon:       L.icon({iconUrl: '/AirQualityMarkers/BadAQ.png',      iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        veryBadAirQualityIcon:   L.icon({iconUrl: '/AirQualityMarkers/VeryBadAQ.png',  iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        unknownAirQualityIcon:   L.icon({iconUrl: '/AirQualityMarkers/UnknownAQ.png',  iconSize: [64, 64], iconAnchor: [32, 64], popupAnchor: [12, -48] }),
+        defaultGreen:            L.icon({iconUrl: 'https://maps.gstatic.com/mapfiles/ms2/micons/green-dot.png', iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [12, -32] }),
+        defaultBlue:             L.icon({iconUrl: 'https://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png', iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [12, -32] }),
+        };
+        const [iconNames, textValues] = await Promise.all([
         fetch('/api/sensor/GetIconDataBatch', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(sensorData)
-        }).then(r => r.json()),
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(sensorData)
+                }).then(r => r.json()),
 
         fetch('/api/sensor/GetPopUpDataBatch', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(sensorData)
-        }).then(r => r.json())
-        
-    ]);
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(sensorData)
+                }).then(r => r.json())
+        ]);
 
-    const markers = L.markerClusterGroup();
+        const markers = L.markerClusterGroup();
 
-    sensorData.forEach((sensor, index) => {
-        let latLng = null;
-        let popupText = "SensorId: " + textValues[index].sensorId + "<br/>" + ((textValues[index].textResults) || "Sensor");
-        const iconName = iconNames[index].icon;
+        sensorData.forEach((sensor, index) => {
+            let latLng = null;
+            let popupText = "SensorId: " + textValues[index].sensorId + "<br/>" + ((textValues[index].textResults) || "Sensor");
+            const iconName = iconNames[index].icon;
         let icon = icons[iconName.toString()] || icons["defaultGreen"];
 
         if (sensor.latitude != null && sensor.longitude != null) {
             latLng = L.latLng(sensor.latitude, sensor.longitude);
-        } else if (sensor.location?.latitude != null && sensor.location?.longitude != null) {
+            } else if (sensor.location?.latitude != null && sensor.location?.longitude != null) {
             latLng = L.latLng(sensor.location.latitude, sensor.location.longitude);
-            icon = icons[iconName.toString()] || icons["defaultBlue"];
-        }
+        icon = icons[iconName.toString()] || icons["defaultBlue"];
+            }
 
         if (latLng) {
-            const marker = L.marker(latLng, { icon }).bindPopup(popupText);
-            markers.addLayer(marker);
-        }
-    });
-    delete sensorData;
-
-    map.addLayer(markers);
-
-    let userLatLng = null;
-    let routingControl = null;
-    let destinationMarker = null;
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            userLatLng = L.latLng(position.coords.latitude, position.coords.longitude);
-
-            L.marker(userLatLng).addTo(map)
-                .bindPopup("Twoja aktualna lokalizacja");
-
-            map.setView(userLatLng, 10);
-        }, function (error) {
-            console.error("Błąd uzyskiwania lokalizacji: ", error);
+                const marker = L.marker(latLng, {icon}).bindPopup(popupText);
+        markers.addLayer(marker);
+            }
         });
-    }
+        delete sensorData;
 
-    map.on('contextmenu', function (e) {
-        if (!userLatLng) {
-            alert("Lokalizacja użytkownika nie jest jeszcze dostępna.");
-            return;
+        map.addLayer(markers);
+
+        let userLatLng = null;
+        let routingControl = null;
+        let destinationMarker = null;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                userLatLng = L.latLng(position.coords.latitude, position.coords.longitude);
+
+                L.marker(userLatLng).addTo(map)
+                    .bindPopup("Twoja aktualna lokalizacja");
+
+                map.setView(userLatLng, 10);
+            }, function (error) {
+                console.error("Błąd uzyskiwania lokalizacji: ", error);
+            });
         }
+
+        map.on('contextmenu', function (e) {
+            if (!userLatLng) {
+            alert("Lokalizacja użytkownika nie jest jeszcze dostępna.");
+        return;
+            }
 
         const destLatLng = e.latlng;
 
         if (destinationMarker) {
             map.removeLayer(destinationMarker);
-        }
+            }
 
         destinationMarker = L.marker(destLatLng).addTo(map)
-            .bindPopup("Nowy punkt docelowy")
-            .openPopup();
+        .bindPopup("Nowy punkt docelowy")
+        .openPopup();
 
         if (routingControl) {
             map.removeControl(routingControl);
-        }
+            }
 
         routingControl = L.Routing.control({
             waypoints: [userLatLng, destLatLng],
-            routeWhileDragging: false,
-            show: false,
-            createMarker: () => null
-        }).addTo(map);
+        routeWhileDragging: false,
+        show: false,
+                createMarker: () => null
+            }).addTo(map);
+        });
     });
-});
