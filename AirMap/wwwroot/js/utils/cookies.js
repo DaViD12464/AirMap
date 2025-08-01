548523ce39ce202ca8955f100311e4c0d673a9ce
Dawid <dawidczernik@interia.pl>
2025-08-01 12:35:59 +0200

    Add comments.

--- AirMap/wwwroot/js/utils/cookies.js
+++ AirMap/wwwroot/js/utils/cookies.js
@@ -1,5 +1,5 @@
 ﻿// wwwroot/js/map/cookies.js
-
+//probably duplicated with cookies.js in wwwroot/js/cookies.js
 export function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
