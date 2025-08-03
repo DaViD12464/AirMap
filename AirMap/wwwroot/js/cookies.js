23f74a52d1a7d08b88325a1b8eb8d0b00e8f7e80
Dawid <dawidczernik@interia.pl>
2025-08-04 01:13:38 +0200

    Add 'rejectedCookies' cookie to stop window from popping-up.

--- AirMap/wwwroot/js/cookies.js
+++ AirMap/wwwroot/js/cookies.js
@@ -14,9 +14,17 @@ const executeCodes = () => {
                 //set cookies for 1month [60= 1min, 60 = 1hour, 24 = 1 day, exdays = value to be defined for how many days cookie should last]
                 const d = new Date();
                 const exdays = 30;
-                d.setTime(d.getTime() + (exdays * 24 * 60 * 60 *1000));
+                d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
                 const expires = `expires=${d.toUTCString()}`;
-                document.cookie =  "cookieBy = AirMap; " + expires + ";path=/";
+                document.cookie = "cookiesAccepted = AirMap; " + expires + ";path=/";
+            }
+            else { //add a cookie when cookies are rejected to stop cookie box from showing again for 1 month
+                console.log("Cookies rejected");
+                const d = new Date();
+                const exdays = 30;
+                d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+                const expires = `expires=${d.toUTCString()}`;
+                document.cookie = "cookiesRejected = AirMap; " + expires + ";path=/";
             }
         });
     });
