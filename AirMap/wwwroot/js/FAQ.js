bcd9e438ab8406c59707e509e93e3ed575cb4508
Dawid <dawidczernik@interia.pl>
2025-08-04 13:25:14 +0200

    Update FAQ menu styling to final version.

--- AirMap/wwwroot/js/FAQ.js
+++ AirMap/wwwroot/js/FAQ.js
@@ -7,10 +7,20 @@
             const answer = question.nextElementSibling;
             const icon = question.querySelector(".icon");
 
-            if (answer.style.display == "block") {
-                answer.style.display = "none";
+            if (answer.classList.contains("hidden")) {
+                answer.classList.remove("hidden");
+                answer.style.transition = "";
+                requestAnimationFrame(() => {
+                    answer.classList.add("open");
+                });
             } else {
-                answer.style.display = "block";
+                answer.style.transition = "max-height 0.6s ease-in-out, padding 0.8s ease-in-out";
+                answer.classList.remove("open");
+                answer.addEventListener("transitionend", function handler() {
+                    answer.classList.add("hidden");
+                    answer.style.transition = "";
+                    answer.removeEventListener("transitionend", handler);
+                });
             }
 
             toggleArrows(icon);
